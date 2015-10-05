6ca60ead016b60bf889a5484e70d2e81233a272b
Phil <ludivig@hotmail.com>
2015-10-05 13:31:27 +0200

    license files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# face_detection
-Face Detection for ROS
+**DISCLAMER:**
+
+This project was created within an academic research setting, and thus should
+be considered as EXPERIMENTAL code. There may be bugs and deficiencies in the
+code, so please adjust expectations accordingly. With that said, we are
+intrinsically motivated to ensure its correctness (and often its performance).
+Please use the corresponding web repository tool (e.g. github, bitbucket, etc)
+to file bugs, suggestions, pull requests; we will do our best to address them
+in a timely manner.
+
+
+**LAYOUT:**
+- face_detection/
+  - cfg/:                 dynamic_reconfigure configuration files
+  - include/:             cascade files for the detection
+  - launch/:              roslaunch files
+  - src/:                 source files
+  - CMakeLists.txt:       CMake project configuration file
+  - LICENSES:             license agreement
+  - package.xml:          ROS/Catkin package file
+  - README.txt:            this file
+
+
+
+
+
+Copyright (c) 2015, Philippe Ludivig
+
+All rights reserved.
+
+BSD2 license: see LICENSE file
--- src/face_detection.cpp
+++ src/face_detection.cpp
@@ -1,3 +1,43 @@
+//##########################################################################
+// DO NOT MODIFY
+//
+//This project was created within an academic research setting, and thus should
+//be considered as EXPERIMENTAL code. There may be bugs and deficiencies in the
+//code, so please adjust expectations accordingly. With that said, we are
+//intrinsically motivated to ensure its correctness (and often its performance).
+//Please use the corresponding web repository tool (e.g. github, bitbucket, etc)
+//to file bugs, suggestions, pull requests; we will do our best to address them
+//in a timely manner.
+//
+// SOFTWARE LICENSE AGREEMENT (BSD LICENSE):
+//
+//
+//Copyright (c) 2015, Philippe Ludivig
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
+//
+//* Redistributions of source code must retain the above copyright notice, this
+//  list of conditions and the following disclaimer.
+//
+//* Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation
+//  and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//##########################################################################
+
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
